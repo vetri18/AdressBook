8db4711931d428ed50db91b1facb8675a1f86c0b
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-30 11:41:31 +0530

    creating contact uc1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace AdressBook 
+﻿namespace AddressBookSystem
 {
     class Program
     {
+        //creating list of contact
+        public static List<Contact> contactList = new List<Contact>();
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Adress Book Problem ");
+            Console.WriteLine("Welcome to Address Book Sytem.");
+
+            //ability to creating contacts
+            Contact contact1 = new Contact();
+            contact1.firstName = "Vidhya";
+            contact1.lastName = "Mundhe";
+            contact1.address = "Jalna Maharashtra";
+            contact1.phone = 1234567890;
+            contact1.city = "Jalna";
+            contact1.state = "Maharashtra";
+            contact1.zipcode = 413515;
+            contactList.Add(contact1);
+
         }
     }
 }
