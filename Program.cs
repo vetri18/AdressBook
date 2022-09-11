91cf1c2e4b69f719a6f0dbe17c4380dd398569b3
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-11 09:24:38 +0530

    getting contacts by state or city uc 19

--- Program.cs
+++ Program.cs
@@ -1,10 +1,66 @@
-﻿namespace AdressBook 
+﻿namespace AddressBook_ADO.NET
 {
+    /// <summary>
+    /// In this program we are going to manage Address Book using DataBase.
+    /// </summary>
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Adress Book Problem ");
+            Console.WriteLine("Welcome to the AddressBook DataBase problem");
+
+
+            ////instatiating address book operations
+            AddressBookOperations addressBookOperations = new AddressBookOperations();
+            ////UC16 getting contact details
+            //try
+            //{
+            //    List<AddressBookContactDetails> contactDetailsList = addressBookOperations.GetAllContactDetails();
+            //    contactDetailsList.ForEach(contactDetails =>
+            //    {
+            //        Console.WriteLine("ContactID:- " + contactDetails.contactID + " First Name:- " + contactDetails.firstName + " Last Name:- " + contactDetails.lastName + " Address:- " + contactDetails.address + " City:- " + contactDetails.city + " State:- " + contactDetails.state + " Zip:- " + contactDetails.zip + " phone number:- " + contactDetails.phoneNo + " Email:- " + contactDetails.eMail);
+            //        Console.WriteLine("address Book Name id: -" + contactDetails.addressBookNameId + " address book name: -" + contactDetails.addressBookName);
+            //        Console.WriteLine("Type id: -" + contactDetails.typeId + " type name: -" + contactDetails.typeName);
+            //    });
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+
+            //UC18 getting contact details in date range
+            //Console.WriteLine("*************");
+            //try
+            //{
+            //    List<AddressBookContactDetails> contactDetailsListInDateRange = addressBookOperations.GetAllContactDetailsForParticularDateRange();
+            //    contactDetailsListInDateRange.ForEach(contactDetails =>
+            //    {
+            //        Console.WriteLine("ContactID:- " + contactDetails.contactID + " First Name:- " + contactDetails.firstName + " Last Name:- " + contactDetails.lastName + " Address:- " + contactDetails.address + " City:- " + contactDetails.city + " State:- " + contactDetails.state + " Zip:- " + contactDetails.zip + " phone number:- " + contactDetails.phoneNo + " Email:- " + contactDetails.eMail);
+            //        //Console.WriteLine("address Book Name id: -" + contactDetails.addressBookNameId + " address book name: -" + contactDetails.addressBookName);
+            //        //Console.WriteLine("Type id: -" + contactDetails.typeId + " type name: -" + contactDetails.typeName);
+            //    });
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            //UC18 getting contact details in date range
+            //UC19 getting contact details for particular city or state
+            Console.WriteLine("*************");
+            try
+            {
+                List<AddressBookContactDetails> contactDetailsListInDateRange = addressBookOperations.GetAllContactDetailsWithConditions();
+                contactDetailsListInDateRange.ForEach(contactDetails =>
+                {
+                    Console.WriteLine("ContactID:- " + contactDetails.contactID + " First Name:- " + contactDetails.firstName + " Last Name:- " + contactDetails.lastName + " Address:- " + contactDetails.address + " City:- " + contactDetails.city + " State:- " + contactDetails.state + " Zip:- " + contactDetails.zip + " phone number:- " + contactDetails.phoneNo + " Email:- " + contactDetails.eMail);
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
